631c1d2775fe9c7f4ba8521231f1dbbf7340784f
Ben Brostoff <ben.brostoff@gmail.com>
2014-08-06 10:21:05 -0400

    UX on controlle

--- week_7/7_gps3.1/BB_SOLUTION_201408/CONTROLLER.rb
+++ week_7/7_gps3.1/BB_SOLUTION_201408/CONTROLLER.rb
@@ -10,10 +10,26 @@ class Controller
   end
 
   def start
-    self.view.start_display
+    view.start_display
+    view.option_display
+    return gets.chomp.to_i
+  end
+
+  def reprompt
+    view.error_display
+    start
+  end
+
+  def route_choose(option)
+    add_item if option == 1
+    display_list if option == 2
+    remove_item if option == 3
+    change_unit if option == 4
+    done if option == 5
+    reprompt if option == 0 || option > 5
   end
 
 end
 
 controller = Controller.new
-controller.start
+controller.route_choose(controller.start)
